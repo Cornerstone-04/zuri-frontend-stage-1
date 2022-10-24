9bf7cf8a944bcabbb2455a3e511d4c4704b25958
Cornerstone <fortunecornerstone@gmail.com>
2022-10-24 21:07:49 +0100

    Create index file for assets
    - Create  file in the assets folder
    - import icons and images
    - export files

--- src/assets/icons/index.jsx
+++
@@ -1,4 +0,0 @@
-import Github from "./github.svg";
-import Slack from "./slack.svg";
-
-export {Github, Slack};
---
+++ src/assets/index.jsx
@@ -0,0 +1,5 @@
+import Github from "./icons/github.svg";
+import Slack from "./icons/slack.svg";
+import ProfilePic from "./images/profile__img.png";
+
+export { Github, Slack, ProfilePic };
