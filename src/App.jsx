355b92857a4670c51ede71c43f8d76b129606786
Cornerstone <fortunecornerstone@gmail.com>
2022-10-24 21:16:39 +0100

    Create profile section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { ProfilePic } from "./assets";
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="bg-zuriBg min-h-screen flex flex-col justify-start items-center gap-8 sm:gap-14 relative">
+      <div className="mt-16 flex flex-col justify-center items-center gap-6">
+        <img src={ProfilePic} alt="" />
+        <p className="font-bold text-zuriGray-900 text-xl font-default">
+          Annette Black
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
